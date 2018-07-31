c7b9d3113ed6b3dd2c6f9ca87d696d9fcc388c54
Afa Madza <305@holbertonschool.com>
2018-07-31 04:22:26 +0000

    Added / to directory name

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # Sets up web servers for deployment of web static
-sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 IDX='/data/web_static/releases/test/index.html'
-echo "<!DOCTYPE html><body><p>Hello World</p></body></html>" | sudo tee $IDX
+sudo echo -e "<!DOCTYPE html><body><p>Hello World</p>
+</body></html>" | sudo tee $IDX
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-STR='location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n'
+STR='location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n'
 sudo sed -i "38i $STR" /etc/nginx/sites-available/default
 sudo service nginx restart
