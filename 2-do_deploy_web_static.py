630d894f83e7964a2a8cfc554c86dec8c1168857
Afa Madza <305@holbertonschool.com>
2018-07-31 04:44:49 +0000

    Fixed typo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,11 +5,30 @@ import os
 from fabric.operations import put
 from datetime import datetime
 from fabric.api import run, env, hosts
+from fabric import operations
 
 
 env.hosts = ["35.237.246.199", "35.196.144.206"]
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/holberton'
+
+
+
+def do_pack():
+    """
+    Generate a tgz file in the local server
+    """
+    curr_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_ = "versions/web_static_{}.tgz".format(curr_time)
+    msg = ''
+    msg += operations.local('mkdir -p versions')
+    msg += operations.local('tar -czvf {} web_static'.format(file_))
+    file_size = os.stat('{}'.format(file_)).st_size
+    output = 'web_static packed: versions/{} -> {}'.format(file_, file_size)
+    try:
+        print(output+'Bytes')
+    except:
+        return None
 
 def do_deploy(archive_path):
     """
@@ -31,7 +50,7 @@ def do_deploy(archive_path):
         '/data/web_static/releases/{}/'.format(arc_dir, arc_dir))
     run('rm -rf /data/web_static/releases/{}/web_static'.format(arc_dir))
     run('rm -rf /data/web_static/current')
-    run('sudo ln -sf /data/web_static/releases/{} /data/web_static/current'
+    run('sudo ln -sf /data/web_static/releases/{}/ /data/web_static/current'
         .format(arc_dir))
     print('New version deployed!')
     return True
