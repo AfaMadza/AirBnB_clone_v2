9d75e8a5f145fb014a9ac763bc7eecba2dbca73f
Afa Madza <305@holbertonschool.com>
2018-08-16 06:59:59 +0000

    Added README

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,25 +14,27 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.route('/states')
 @app.route('/states_list')
 def states_list():
     """
     Returns State data from database and insert into template
     """
-    state_inst = storage.all('States').values()
+    state_inst = storage.all('State').values()
     state_data = dict([state.name, state.id] for state in state_inst)
     return render_template('7-states_list.html', state_data=state_data)
 
 
-@app.route('/cities_by_states')
-def display_cities():
+@app.route('/states/<id>')
+def display_state_via_id(id):
     """
     Returns State data from database and insert into template
     """
-    state_inst = storage.all('State').values()
-    state_data = dict([state.name, state] for state in state_inst)
-    return render_template('8-cities_by_states.html', state_data=state_data)
-
+    st = None
+    for state_obj in storage.all('State').values():
+        if state_obj.id == id:
+            st = state_obj
+    return render_template('9-states.html', data=storage.all('State'), st=st)
 
 @app.teardown_appcontext
 def teardown_db(exception):
